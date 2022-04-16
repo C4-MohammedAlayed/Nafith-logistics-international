ff1d5bcf10165bceedc5581958839d7a089bfae3
MohammedAlayed <alayedmohammed0@gmail.com>
2022-04-16 12:50:16 +0300

    port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("./db/db");
 const path = require("path");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Import Routers
 const rolesRouter = require("./routes/roles");
